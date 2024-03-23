72a97d9168ba93f3b9fbe25376ebc3c96cb8897b
EucTech <euccreative@gmail.com>
2024-03-23 03:40:49 +0100

    created Item component that will be used to map all products

---
+++ frontend/src/Components/Items/Items.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Items.css';
+
+
+
+const Items = () => {
+  return (
+    <div className='items'>
+      <div className="items-main">
+      
+      </div>
+
+    </div>
+  )
+}
+
+export default Items
