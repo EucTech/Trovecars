1b0d2e9d82c8f60d9413adf8e04c117030fae201
EucTech <euccreative@gmail.com>
2024-03-23 03:45:18 +0100

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
+import Items from './Components/Items/Items'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Navbar/>
+        <Items/>
       </BrowserRouter>
     </div>
   )
