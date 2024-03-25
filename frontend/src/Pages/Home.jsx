ff02ab9807aa0a61774d4de465d4ce05e2b957c8
EucTech <euccreative@gmail.com>
2024-03-25 20:37:37 +0100

    updated

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import FeaturedListing from "../Components/FeaturedListing/FeaturedListing";
 import Navbar from "../Components/Navbar/Navbar";
+import Popular from "../Components/Popular/Popular";
 
 const Home = () => {
   return (
     <div className="Home">
       <Navbar />
       <FeaturedListing />
+      <Popular/>
     </div>
   );
 };
