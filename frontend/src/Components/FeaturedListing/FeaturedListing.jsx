86b91dd07e2f0849e2d1cd24e2c1dfd5c35dbbeb
EucTech <euccreative@gmail.com>
2024-03-25 23:56:27 +0100

    updated updated featured Listing to filter products based on the make

--- frontend/src/Components/FeaturedListing/FeaturedListing.jsx
+++ frontend/src/Components/FeaturedListing/FeaturedListing.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FeaturedListing.css";
 import { Button } from "@material-tailwind/react";
 import all_products from "../Assets/all_products";
@@ -7,53 +7,60 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImages } from "@fortawesome/free-solid-svg-icons";
 
 const FeaturedListing = () => {
+
+  const [carCondition, setCarCondition] = useState("New");
+
+  const filterCondition = all_products.filter(
+    (item) => item.condition === carCondition
+  );
+
   return (
     <div className="featuredlisting">
       <div className="featuredlisting-header">
         <h1>Featured Listings</h1>
         <div className="featuredlisting-new-used">
-          <Button className="featuredlisting-btn fl-btn-active">New</Button>
-          <Button className="featuredlisting-btn">Used</Button>
+          <Button className={`featuredlisting-btn ${carCondition === "New" ? "fl-btn-active" : ""}`} onClick={()=>setCarCondition("New")}>New</Button>
+          <Button className={`featuredlisting-btn ${carCondition === "Used" ? "fl-btn-active" : ""}`} onClick={()=>setCarCondition("Used")}>Used</Button>
         </div>
       </div>
       <div className="featuredlisting-main">
         <div className="featuredlisting-main-left">
           <div
             className="featuredlisting-left-img-section"
-            key={all_products[0].id}
-            id={all_products[0].id}
+            key={filterCondition[0].id}
+            id={filterCondition[0].id}
           >
             <div
               className="featuredlisting-left-image"
               style={{
                 backgroundImage: `url(${
-                  Object.values(all_products[0].images)[0]
+                  Object.values(filterCondition[0].images)[0]
                 })`,
               }}
             ></div>
             <div className="featuredlisting-left-numberof-images">
               <FontAwesomeIcon className="image-icon" icon={faImages} />
-              <p>{Object.values(all_products[0].images).length}</p>
+              <p>{Object.values(filterCondition[0].images).length}</p>
             </div>
           </div>
           <div className="featuredlisting-left-info">
-            <h4>{all_products[0].title}</h4>
+            <h4>{filterCondition[0].title}</h4>
             <hr />
             <div className="featuredlisting-tran">
               <div className="featured-type">
-                <Button className="featured-btn">{all_products[0].year}</Button>
-                <h5>{all_products[0].transmission}</h5>
-                <h5 className="fuel-type">{all_products[0].fuel_type}</h5>
-                <h5 className="drive-type">{all_products[0].drive_type}</h5>
+                <Button className="featured-btn">{filterCondition[0].year}</Button>
+                <h5>{filterCondition[0].transmission}</h5>
+                <h5 className="fuel-type">{filterCondition[0].fuel_type}</h5>
+                <h5 className="drive-type">{filterCondition[0].drive_type}</h5>
               </div>
               <div className="featured-price">
-                <h3>&#x20a6;{all_products[0].price}</h3>
+                <h3>&#x20a6;{filterCondition[0].price}</h3>
               </div>
             </div>
           </div>
         </div>
         <div className="featuredlisting-main-right">
-          {all_products.slice(1, 4).map((item, i) => {
+          {filterCondition.slice(1, 5).map((item, i) => {
             return (
               <Items
                 key={i}
