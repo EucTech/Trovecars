cf19942b855d7eae02959f4e24f63397161e82a0
EucTech <euccreative@gmail.com>
2024-03-27 05:16:27 +0100

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Home from './Pages/Home'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Home/>
+        <Footer/>
       </BrowserRouter>
     </div>
   )
