ce6e5ba8c2335e9c94a696e52f814940387c226b
EucTech <euccreative@gmail.com>
2024-03-27 12:32:17 +0100

    created Breadcrum page

---
+++ frontend/src/Components/Breadcrum/Breadcrum.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Breadcrum.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
+
+const Breadcrum = (props) => {
+  const { product } = props;
+  return (
+    <div className="breadcrum">
+      <p><Link to="/">Home </Link></p>
+      <FontAwesomeIcon className="b-icon" icon={faChevronRight} />
+      <p><Link to="/cars">Cars</Link></p>
+      <FontAwesomeIcon className="b-icon" icon={faChevronRight} />
+      <p><Link to={`/cars/${product.make}`}>{product.make}</Link></p>
+      <FontAwesomeIcon className="b-icon" icon={faChevronRight} />
+      <p><Link to={`/cars/${product.model}`}>{product.model}</Link></p>
+      <FontAwesomeIcon className="b-icon" icon={faChevronRight} />
+      <h5>{product.title}</h5>
+    </div>
+  );
+};
+
+export default Breadcrum;
