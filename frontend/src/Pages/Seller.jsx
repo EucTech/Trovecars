88d31b64f2e210751b7d4045015dd89270ab410b
EucTech <euccreative@gmail.com>
2024-03-31 02:13:35 +0100

    created seller page

---
+++ frontend/src/Pages/Seller.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainNavbar from '../Components/MainNavbar/MainNavbar'
+
+const Seller = () => {
+  return (
+    <div className='seller'>
+      <MainNavbar/>
+    </div>
+  )
+}
+
+export default Seller
