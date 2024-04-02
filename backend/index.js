4090e857adb157c33848c4cec8c169a9b0538ade
EucTech <euccreative@gmail.com>
2024-04-02 20:00:34 +0100

    created index

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+const {error} = require("console");
+
+const port = process.env.PORT || 4000;
+const mongoString = process.env.DATABASE_URL;
+
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+
+// Connect to MongoDB
+mongoose.connect(mongoString);
+
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.error('MongoDB connection error:', error);
+});
+
+database.once('open', () => {
+    console.log('Database connected');
+});
+
+// Routes
+const router = require('./routes/index');
+app.use('/api', router);
+
+// Start the server
+app.listen(port, (error) => {
+	if (!error) {
+		console.log("Serving Running on Port " + port)
+	} else {
+		console.log("Error: " + error)
+	}
+})
+
